edec08d0dda4f106c452a4b46c296536074de21d
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-08-07 18:53:29 -0400

    Added Pop, At, Tail methods

--- app.js
+++ app.js
@@ -50,15 +50,29 @@ class LinkedList {
     };
 
     tail() {
-
+        let pointer = this.headNode;
+        while (pointer.nextNode !== null) {
+            pointer = pointer.nextNode;
+        };
+        return pointer;
     };
 
     at(index) {
-
+        let pointer = this.headNode;
+        for (let i = 0; i < index; i++) {
+          pointer = pointer.nextNode;
+        };
+        return pointer;
     };
 
     pop() {
-
+        if (this.headNode.nextNode === null) {
+            headNode = null;
+            this.length--;
+          } else {
+            this.at(this.size() - 2).nextNode = null;
+            this.length--;
+          };
     };
 
     contains(value) {
@@ -76,7 +90,8 @@ class LinkedList {
 
 const newList = new LinkedList();
 newList.prepend(1);
-newList.append(1);
+newList.append(5);
+newList.pop();
 console.log(newList);
 
 // node app.js
