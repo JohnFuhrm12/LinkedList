2ed1addf4dfb2d63bb59a8f13cc4564ea7fdab05
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-08-07 17:45:34 -0400

    Init

---
+++ app.js
@@ -0,0 +1,19 @@
+class Node {
+    constructor(value) {
+        this.value = value || null;
+        this.nextNode = null;
+    };
+};
+
+class LinkedList {
+    constructor() {
+        this.headNode = null;
+        this.tailNode = null;
+    };
+
+    append(value) {
+        
+    };
+};
+
+// node app.js
